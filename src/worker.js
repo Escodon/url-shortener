a8a3eea378e7584da7c26f38ccf36b0cf41f342b
NiceygyLive <niceygy@niceygylive.xyz>
2023-10-12 18:54:25 +0000

    Finished!

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,7 @@
 async function handleRequest(url, KV) {
   console.log("Handling request for:", url);
   if (url == "") {
-    return "Please enter a URL", { status: 200 };
+    return new Response("Please enter a URL", { status: 200 });
   }
   if (url.includes("favicon.ico")) {
     console.log("favicon.ico requested");
@@ -11,16 +11,18 @@ async function handleRequest(url, KV) {
 	let newUrl = await KV.get(url);
 	if (newUrl == null) {
 		console.log("Slug not found in DB. Returning 404");
-		return "Slug not found", { status: 404 };
+		return new Response("Slug not found", { status: 404 });
 	}
 	console.log("Slug found in DB("+newUrl+"). Returning 301");    
-      return (
-        newUrl,
-        {
-          status: 301,
-          headers: { Location: newUrl },
-        }
-      );
+	return Response.redirect(
+		"https://"+newUrl,
+		301,
+		{
+		  headers: {
+			"Cache-Control": "no-store",
+		  },
+		}
+	  );
   }
 }
 
@@ -29,6 +31,6 @@ export default {
     var url = request.url;
     var url = url.split("/")[3];
     let KV = env.KV
-    return new Response(await handleRequest(url, KV));
+    return (await handleRequest(url, KV));
   },
 };
