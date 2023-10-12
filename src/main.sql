25c89a9ec7577b8faa25d99721b84f24efd3b61a
NiceygyLive <niceygy@niceygylive.xyz>
2023-10-12 16:23:25 +0000

    switch to d1, pt.1

---
+++ src/main.sql
@@ -0,0 +1,2 @@
+SELECT * FROM links WHERE slug = 'hallo';
+UPDATE links SET count = '1' WHERE slug = 'hallo';
--- src/worker.js
+++ src/worker.js
@@ -1,15 +1,22 @@
-async function handleRequest(url, Links, Count) {
+async function find(slug, DB) {
+	let resFromDB = await DB.prepare("SELECT * FROM links WHERE slug = '"+slug+"'").get(slug);
+	await DB.prepare("UPDATE links SET count = count + 1 WHERE slug = '"+slug+"'").run();
+	return resFromDB; //array i think
+  }
+
+async function handleRequest(url, DB) {
 	if (url.includes("favicon.ico")) {
 		return (null, { status: 404 });
 	}
 	if (url.includes("/new")) {
 		let newUrl = url.split("?url=")[1];
 		let newUrlSlug = url.split("?slug=")[1];
-		await Links.put(newUrlSlug, newUrl);
+		await find(newUrlSlug, newUrl, DB)[2] //Array = Slug: Link: Count
 		return ("OK", { status: 200 });
 	} else {
 		let slug = url.split("/")[1];
-		let newUrl = await Links.get(slug);
+
+		//let newUrl = await Links.get(slug);
 		if (newUrl) {
 			await Count.put(slug, 1);
 			return new Response(newUrl, {
@@ -23,11 +30,16 @@ async function handleRequest(url, Links, Count) {
 
 	
 }
+
+
 export default {
 	async fetch(request, env, ctx) {
 		let url = request.url;
-		let Links = env.Links;
-		let Count = env.Count;
-		return new Response(handleRequest(url, Links, Count));
+		let DB = env.DB
+
+		//DB Consts
+		
+
+		return new Response(handleRequest(url, DB));
 	},
 };
